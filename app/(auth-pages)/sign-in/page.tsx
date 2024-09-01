6dfef9ada59e16ee8a8b1d8e80a3c9188a505f3d
S.Niknejad <S.Niknejad@flightio.com>
2024-09-01 16:53:16 +0330

    feat: persian login page

--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -8,23 +8,23 @@ import Link from "next/link";
 export default function Login({ searchParams }: { searchParams: Message }) {
   return (
     <form className="flex-1 flex flex-col min-w-64">
-      <h1 className="text-2xl font-medium">Sign in</h1>
+      <h1 className="text-2xl font-medium">ورود کاربر</h1>
       <p className="text-sm text-foreground">
-        Don't have an account?{" "}
+        حساب کاربری ندارید؟{" "}
         <Link className="text-foreground font-medium underline" href="/sign-up">
-          Sign up
+          ساختن حساب
         </Link>
       </p>
       <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
-        <Label htmlFor="email">Email</Label>
+        <Label htmlFor="email">ایمیل</Label>
         <Input name="email" placeholder="you@example.com" required />
         <div className="flex justify-between items-center">
-          <Label htmlFor="password">Password</Label>
+          <Label htmlFor="password">رمز ورود</Label>
           <Link
             className="text-xs text-foreground underline"
             href="/forgot-password"
           >
-            Forgot Password?
+            رمز عبور را فراموش کرده اید؟
           </Link>
         </div>
         <Input
@@ -33,8 +33,8 @@ export default function Login({ searchParams }: { searchParams: Message }) {
           placeholder="Your password"
           required
         />
-        <SubmitButton pendingText="Signing In..." formAction={signInAction}>
-          Sign in
+        <SubmitButton pendingText="در حال ورود..." formAction={signInAction}>
+          ورود
         </SubmitButton>
         <FormMessage message={searchParams} />
       </div>
