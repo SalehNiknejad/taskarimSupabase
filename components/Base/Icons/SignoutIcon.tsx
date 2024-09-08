463cdcf02f7bee50981967b6df418a9d14d5997b
S.Niknejad <S.Niknejad@flightio.com>
2024-09-08 11:39:51 +0330

    fix: change icon size

--- components/Base/Icons/SignoutIcon.tsx
+++ components/Base/Icons/SignoutIcon.tsx
@@ -2,8 +2,8 @@ export default function SignoutIcon() {
   return (
     <svg
       aria-label="signout icon"
-      width="128"
-      height="128"
+      width="24"
+      height="24"
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
