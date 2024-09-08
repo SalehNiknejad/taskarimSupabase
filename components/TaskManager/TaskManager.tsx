740896f608fa2e5cd44593e02680677c55995c2a
S.Niknejad <S.Niknejad@flightio.com>
2024-09-08 18:50:15 +0330

    feat: add color style and fix get data for each user

--- components/TaskManager/TaskManager.tsx
+++ components/TaskManager/TaskManager.tsx
@@ -1,5 +1,6 @@
 import { createClient } from "@/utils/supabase/server";
 import React from "react";
+import { Label } from "../ui/label";
 
 async function TaskManager() {
   const {
@@ -25,13 +26,19 @@ async function TaskManager() {
     <>
       <div className="flex flex-wrap gap-2">
         {data?.map((item: displayReq) => (
-          <div className="w-64 h-48 border border-1 rounded-lg p-3 flex-col justify-between">
-            <h2 className="font-extrabold text-wrap">{item.title}</h2>
-            <p className="font-normal text-sm">{item.description}</p>
-            <p className="font-light text-sm">{item.date}</p>
-            <p className="font-light text-sm">
-              {item.isCompleted ? "انجام شده" : "انجام نشده"}
-            </p>
+          <div className="w-72 h-48 border border-1 rounded-lg p-3 flex flex-col justify-between">
+            <div>
+              <h2 className="font-extrabold text-wrap">{item.title}</h2>
+              <p className="font-normal text-sm">{item.description}</p>
+            </div>
+            <div>
+              <p className="font-light text-sm mb-1">{item.date}</p>
+              <Label
+                className={`font-medium text-sm py-0.5 px-3 rounded-2xl ${item.isCompleted ? "bg-green-100 text-green-600 dark:bg-green-900 dark:text-green-100 " : "bg-red-100 text-red-600 dark:bg-red-900 dark:text-red-100"}`}
+              >
+                {item.isCompleted ? "انجام شده" : "انجام نشده"}
+              </Label>
+            </div>
           </div>
         ))}
       </div>
