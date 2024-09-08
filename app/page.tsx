145e02c3057afa66838d920d940426cc6049fb90
S.Niknejad <S.Niknejad@flightio.com>
2024-09-08 18:01:26 +0330

    feat: use taskManager in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import TaskManager from "@/components/TaskManager/TaskManager";
 import Hero from "@/components/hero";
+import { createClient } from "@/utils/supabase/server";
 
 export default async function Index() {
-  return (
-    <>
-      <Hero />
-    </>
-  );
+  const supabase = createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  return <>{user ? <TaskManager /> : <Hero />}</>;
 }
