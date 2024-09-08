b8269037cf8695e9a2d3b5c6354698b4a1ea9b7d
S.Niknejad <S.Niknejad@flightio.com>
2024-09-08 11:14:39 +0330

    feat: add logo in header

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,12 @@ import HeaderAuth from "@/components/header-auth";
 import { ThemeSwitcher } from "@/components/theme-switcher";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import { Vazirmatn } from "next/font/google";
+import LogoType from "@/public/TaskarimLogo.png";
 
 import { ThemeProvider } from "next-themes";
 import Link from "next/link";
 import "./globals.css";
+import Image from "next/image";
 
 const vazir = Vazirmatn({ subsets: ["arabic", "latin", "latin-ext"] });
 
@@ -44,12 +46,20 @@ export default function RootLayout({
             <div className="flex-1 w-full flex flex-col gap-20 items-center">
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-                  <div className="flex gap-5 items-center font-semibold">
-                    <Link href={"/"} className="text-xl">
+                  <div className="flex  items-center font-semibold">
+                    <Link
+                      href={"/"}
+                      className="text-xl flex items-center gap-2"
+                    >
+                      <Image
+                        alt="taskarim"
+                        src={LogoType}
+                        className="dark:brightness-150"
+                        width={36}
+                      />
                       تسکریم
                     </Link>
-                    <div className="flex items-center gap-2">
-                      {/* <DeployButton /> */}
+                    <div className="mr-3">
                       <ThemeSwitcher />
                     </div>
                   </div>
