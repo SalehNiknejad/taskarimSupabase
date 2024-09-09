25540945020c6d8d458435cfaedb4115114eba5d
S.Niknejad <S.Niknejad@flightio.com>
2024-09-09 19:51:54 +0330

    feat: improve responsive performance

--- components/TaskManager/TaskManager.tsx
+++ components/TaskManager/TaskManager.tsx
@@ -1,15 +1,18 @@
-import { createClient } from "@/utils/supabase/server";
 import React from "react";
+import { createClient } from "@/utils/supabase/server";
 import { Label } from "../ui/label";
 import { Button } from "../ui/button";
 import { PlusCircleIcon } from "lucide-react";
+import Link from "next/link";
 
 async function TaskManager() {
   const {
     data: { user },
   } = await createClient().auth.getUser();
+
   const supabase = createClient();
-  type displayReq = {
+
+  type TaskReadProps = {
     id: number;
     date: string;
     title: string;
@@ -19,25 +22,27 @@ async function TaskManager() {
   };
 
   const { data } = await supabase
-    .from("AllTasks")
+    .from("allTask")
     .select("*")
     .eq("authorID", user?.id);
-  // const displayname: displayReq = data?.findLast((a) => a);
+
   return (
     <>
       <main className="w-full">
-        <Button
-          type="button"
-          variant={"secondary"}
-          size={"sm"}
-          className="mb-2"
-        >
-          <PlusCircleIcon size={24} className="me-1" />
-          <span> مورد جدید</span>
-        </Button>
+        <a href="/add-task">
+          <Button
+            type="button"
+            variant={"secondary"}
+            size={"sm"}
+            className="mb-2"
+          >
+            <PlusCircleIcon size={24} className="me-1" />
+            <span> مورد جدید</span>
+          </Button>
+        </a>
         <div className="flex flex-wrap gap-2">
-          {data?.map((item: displayReq) => (
-            <div className="max-sm:w-full sm:w-72 h-48 border border-1 rounded-lg p-3 flex flex-col justify-between">
+          {data?.map((item: TaskReadProps) => (
+            <div className="max-sm:w-full lg:w-[32.7%]	max-lg:w-72 h-48 border border-1 rounded-lg p-3 flex flex-col justify-between">
               <div>
                 <h2 className="font-extrabold text-wrap">{item.title}</h2>
                 <p className="font-normal text-sm">{item.description}</p>
