f5bd99074b993b11b7a85939085e3cce8425781e
S.Niknejad <S.Niknejad@flightio.com>
2024-09-09 17:13:36 +0330

    refactor: sort import

--- app/displaynameset/page.tsx
+++ app/displaynameset/page.tsx
@@ -1,7 +1,7 @@
+import React from "react";
 import { SubmitButton } from "@/components/submit-button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import React from "react";
 import { changeDisplayName } from "../actions";
 
 function displaynameset() {
