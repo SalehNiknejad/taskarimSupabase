88a389fd95e068e5dc25710b1717a0031dbbf9cf
S.Niknejad <S.Niknejad@flightio.com>
2024-09-09 17:13:18 +0330

    feat: add button in TaskManager page for home page

--- components/TaskManager/TaskManager.tsx
+++ components/TaskManager/TaskManager.tsx
@@ -1,6 +1,8 @@
 import { createClient } from "@/utils/supabase/server";
 import React from "react";
 import { Label } from "../ui/label";
+import { Button } from "../ui/button";
+import { PlusCircleIcon } from "lucide-react";
 
 async function TaskManager() {
   const {
@@ -23,24 +25,35 @@ async function TaskManager() {
   // const displayname: displayReq = data?.findLast((a) => a);
   return (
     <>
-      <div className="flex flex-wrap gap-2">
-        {data?.map((item: displayReq) => (
-          <div className="w-72 h-48 border border-1 rounded-lg p-3 flex flex-col justify-between">
-            <div>
-              <h2 className="font-extrabold text-wrap">{item.title}</h2>
-              <p className="font-normal text-sm">{item.description}</p>
+      <main className="w-full">
+        <Button
+          type="button"
+          variant={"secondary"}
+          size={"sm"}
+          className="mb-2"
+        >
+          <PlusCircleIcon size={24} className="me-1" />
+          <span> مورد جدید</span>
+        </Button>
+        <div className="flex flex-wrap gap-2">
+          {data?.map((item: displayReq) => (
+            <div className="max-sm:w-full sm:w-72 h-48 border border-1 rounded-lg p-3 flex flex-col justify-between">
+              <div>
+                <h2 className="font-extrabold text-wrap">{item.title}</h2>
+                <p className="font-normal text-sm">{item.description}</p>
+              </div>
+              <div>
+                <p className="font-light text-sm mb-1">{item.date}</p>
+                <Label
+                  className={`font-medium text-sm py-0.5 px-3 rounded-2xl ${item.isCompleted ? "bg-green-100 text-green-600 dark:bg-green-900 dark:text-green-100 " : "bg-red-100 text-red-600 dark:bg-red-900 dark:text-red-100"}`}
+                >
+                  {item.isCompleted ? "انجام شده" : "انجام نشده"}
+                </Label>
+              </div>
             </div>
-            <div>
-              <p className="font-light text-sm mb-1">{item.date}</p>
-              <Label
-                className={`font-medium text-sm py-0.5 px-3 rounded-2xl ${item.isCompleted ? "bg-green-100 text-green-600 dark:bg-green-900 dark:text-green-100 " : "bg-red-100 text-red-600 dark:bg-red-900 dark:text-red-100"}`}
-              >
-                {item.isCompleted ? "انجام شده" : "انجام نشده"}
-              </Label>
-            </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      </main>
     </>
   );
 }
